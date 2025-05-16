8bec8a978d7750a87b3d56783c11dca38be52006
Vinay Bibyan <vinaybibyan09@gmail.com>
2025-05-17 00:06:56 +0530

    server.js updated

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ try {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist'));
   
-  app.get('/{*any}', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
   });
 }
